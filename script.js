e1ee6881d4adc78b625d5a86f9842b966fee952b
Oto Okino <108208574+oto-okino@users.noreply.github.com>
2024-02-10 05:52:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let function1 = document.getElementById("function1").textContent;
 let function2 = document.getElementById("function2").textContent;
 let function3 = document.getElementById("function3").textContent;
+let functionAny = document.getElementById("input-text").value;
 
 let result = document.getElementById("result");
 
@@ -10,9 +11,36 @@ function testFunction(functionUrl) {
     let request = new XMLHttpRequest();
     request.open('GET', functionUrl, true);
 
-    request.onload = function () {
+    request.onload = () => {
         let data = this.response;
         result.textContent = data
+    }
+
+    request.onerror = () => {
+        result.textContent = "Error";
+    }
+    
+    request.send();
+}
+
+function testFunctionAny(functionUrl) {
+
+    functionAny = document.getElementById("input-text").value;
+
+    functionUrl = functionAny
+
+    let request = new XMLHttpRequest();
+    result.textContent = "Please wait... Excuting now...";
+
+    request.open('GET', functionUrl, true);
+
+    request.onload = () => {
+        let data = this.response;
+        result.textContent = data;
+    }
+
+    request.onerror = () => {
+        result.textContent = "Error";
     }
     
     request.send();
